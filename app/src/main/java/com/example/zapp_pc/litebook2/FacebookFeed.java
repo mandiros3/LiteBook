11528f37f0bb3b8cedc4821717b886e2c76a6f3d
mandiros3 <Mandirosa3@gmail.com>
2015-06-17 03:37:02 -0400

    User is now able to view their facebook feed.

--- app/src/main/java/com/example/zapp_pc/litebook2/FacebookFeed.java
+++ app/src/main/java/com/example/zapp_pc/litebook2/FacebookFeed.java
@@ -1,38 +1,27 @@
 package com.example.zapp_pc.litebook2;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
-public class FacebookFeed extends ActionBarActivity {
+public class FacebookFeed extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_feed);
+
+
+        WebView feed = (WebView) findViewById(R.id.feedView);
+        feed.loadUrl("http://www.facebook.com");
+        WebSettings webSettings = feed.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_facebook_feed, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/zapp_pc/litebook2/OpenChat.java
+++ app/src/main/java/com/example/zapp_pc/litebook2/OpenChat.java
@@ -1,38 +1,26 @@
 package com.example.zapp_pc.litebook2;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
-public class OpenChat extends ActionBarActivity {
+public class OpenChat extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_chat);
+
+        WebView chat = (WebView) findViewById(R.id.chatView);
+        chat.loadUrl("http://www.messenger.com");
+        WebSettings webSettings = chat.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_open_chat, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
